144aff1b7402194e2a1bb69236dfbcc98397bffd
zaal <91364635+zalfaaulia@users.noreply.github.com>
2022-05-18 07:48:09 +0700

    Add files via upload

---
+++ Book_Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.tugas_oop_170522;
+
+/**
+ *
+ * @author Lenovo X380 Yoga
+ */
+public class Book_Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Novel novel1 = new Novel();
+        novel1.setTitle("Bagai Pungguk Merindukan Bulan");
+    }
+    
+}
